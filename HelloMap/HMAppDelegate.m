2e149e393ee9a1808c70dd347ab0ad0cea1d22e3
skarEE <lukemahe@gmail.com>
2013-06-04 16:20:08 +1000

    Update HMAppDelegate.m

--- HelloMap/HMAppDelegate.m
+++ HelloMap/HMAppDelegate.m
@@ -1,23 +1,21 @@
 #import "HMAppDelegate.h"
-
 #import "HMViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
 
 @implementation HMAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    //Add in your API key here:
-    [GMSServices provideAPIKey:@"YOUR_API_KEY"];
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        self.viewController = [[HMViewController alloc] initWithNibName:@"HMViewController_iPhone" bundle:nil];
-    } else {
-        self.viewController = [[HMViewController alloc] initWithNibName:@"HMViewController_iPad" bundle:nil];
-    }
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
-    return YES;
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window =
+      [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+  //Add in your API key here:
+  [GMSServices provideAPIKey:@"YOUR_API_KEY"];
+
+  self.viewController = [[HMViewController alloc] init];
+  self.window.rootViewController = self.viewController;
+  [self.window makeKeyAndVisible];
+  return YES;
 }
 
 @end
