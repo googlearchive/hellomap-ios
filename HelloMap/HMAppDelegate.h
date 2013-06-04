b79e1ab252d08a67337136708f0f9c9ccd9f30ac
skarEE <lukemahe@gmail.com>
2013-06-04 16:20:28 +1000

    Update HMAppDelegate.h

--- HelloMap/HMAppDelegate.h
+++ HelloMap/HMAppDelegate.h
@@ -5,7 +5,6 @@
 @interface HMAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 @property (strong, nonatomic) HMViewController *viewController;
 
 @end
