00f7ebe0e7d342d29698e61f3c1ef24561b082a7
skarEE <lukemahe@gmail.com>
2013-06-04 16:18:02 +1000

    Update main.m

--- HelloMap/main.m
+++ HelloMap/main.m
@@ -1,10 +1,8 @@
 #import <UIKit/UIKit.h>
-
 #import "HMAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HMAppDelegate class]));
-    }
+int main(int argc, char *argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([HMAppDelegate class]));
+  }
 }
