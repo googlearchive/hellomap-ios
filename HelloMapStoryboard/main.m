249edec763ddef48a49655d3241aaea1768b69b3
Brett Morgan <brettmorgan@google.com>
2015-08-11 12:04:16 +1000

    Tidyup

--- HelloMapStoryboard/main.m
+++ HelloMapStoryboard/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  HelloMapStoryBoard
-//
-//  Created by Brett Morgan on 8/11/15.
-//  Copyright (c) 2015 Brett Morgan. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "HMSBAppDelegate.h"
 
